9da64fcf676306c8c075598026bc10b248d7db09
Ziemas <ziemas@ziemas.se>
2025-05-28 12:36:10 +0200

    get rid of inline asm

--- src/ZunMath.hpp
+++ src/ZunMath.hpp
@@ -54,38 +54,46 @@ ZUN_ASSERT_SIZE(ZunVec3, 0xC);
 
 #define RADIANS(degrees) ((degrees * ZUN_PI / 180.0f))
 
-#define sincos(in, out_sine, out_cosine)                                                                               \
-    {                                                                                                                  \
-        __asm { \
-        __asm fld in \
-        __asm fsincos \
-        __asm fstp out_cosine \
-        __asm fstp out_sine }                                            \
-    }
-
 void __inline fsincos_wrapper(f32 *out_sine, f32 *out_cosine, f32 angle)
 {
-    __asm {
-        fld [angle]
-        fsincos
-        mov eax, [out_cosine]
-        fstp [eax]
-        mov eax, [out_sine]
-        fstp [eax]
-    }
+    f32 sin, cos;
+    sin = std::sin(angle);
+    cos = std::cos(angle);
+
+    *out_sine = sin;
+    *out_cosine = cos;
+
+
+    //__asm {
+    //    fld [angle]
+    //    fsincos
+    //    mov eax, [out_cosine]
+    //    fstp [eax]
+    //    mov eax, [out_sine]
+    //    fstp [eax]
+    //}
 }
 
-void __inline sincosmul(D3DXVECTOR3 *out_vel, f32 input, f32 multiplier)
+#define sincos(in, out_sine, out_cosine) fsincos_wrapper(&out_sine, &out_cosine, in)
+
+void __inline sincosmul(zD3DXVECTOR3 *out_vel, f32 input, f32 multiplier)
 {
-    __asm {
-        mov eax, out_vel
-        fld input
-        fsincos
-        fmul [multiplier]
-        fstp [eax]
-        fmul [multiplier]
-        fstp [eax+4]
-    }
+    f32 sin, cos;
+    fsincos_wrapper(&sin, &cos, input);
+
+    out_vel->x = cos * multiplier;
+    out_vel->y = sin * multiplier;
+
+
+    //__asm {
+    //    mov eax, out_vel
+    //    fld input
+    //    fsincos
+    //    fmul [multiplier]
+    //    fstp [eax]
+    //    fmul [multiplier]
+    //    fstp [eax+4]
+    //}
 }
 
 f32 __inline invertf(f32 x)
@@ -95,10 +103,11 @@ f32 __inline invertf(f32 x)
 
 f32 __inline rintf(f32 float_in)
 {
-    __asm {
-        fld float_in
-        frndint
-        fstp float_in
-    }
-    return float_in;
+    return float_in >= 0.0f ? floorf(float_in + 0.5f) : ceilf(float_in - 0.5f);
+    //__asm {
+    //    fld float_in
+    //    frndint
+    //    fstp float_in
+    //}
+    //return float_in;
 }
